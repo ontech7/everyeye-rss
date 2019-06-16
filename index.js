59ed879448cdb9a29c6bcd877602c3a71f25f897
ontech7 <dontrok1@hotmail.it>
2019-06-16 09:23:59 +0200

    Fixed strings for Linux environment (now works with all OS)

--- index.js
+++ index.js
@@ -3,12 +3,16 @@ const cheerio = require('cheerio');
 
 const { JSDOM } = jsdom;
 
-var news_maxLength = 30;
+var news_maxLength = 5;
 var news_title = [];
 var news_description = [];
 var news_link = [];
 var news_creator = [];
 var news_pubDate = [];
+
+var patterns = [/(<!\-\-\[CDATA\[|\]\]\-\->)/g, 
+    /(<!\-\-\[CDATA\[|\]\]\-\->|<[^br].*?>|<br\/>|<br><br><a.*?>.*?<\/a>)/g,
+    /<br><br>/g];
 
 JSDOM.fromURL("https://www.everyeye.it/feed/feed_news_rss.asp", { referrer: "https://www.everyere.it/feed/feed_news_rss.asp" }).then(dom => {
     const $ = cheerio.load(dom.serialize());
@@ -16,12 +20,17 @@ JSDOM.fromURL("https://www.everyeye.it/feed/feed_news_rss.asp", { referrer: "htt
     var articles = $('item');
 
     for(var i = (news_maxLength > articles.length ? articles.length : news_maxLength); i >= 0; i--) {
-        news_title[i] = ($($(articles[i]).find('title')[0]).text()).replace(/(<!\[CDATA\[|\]\]>)/g, "");
-        news_description[i] = ($($(articles[i]).find('description')[0]).text()).replace(/(<!\[CDATA\[|\]\]>)/g, "");
+        news_title[i] = ($($(articles[i]).find('title')[0]).html()).replace(patterns[0], "");
+        news_description[i] = (($($(articles[i]).find('description')[0]).html()).replace(patterns[1], "")).replace(patterns[2], "\n");
         news_link[i] = $($(articles[i]).find('guid')[0]).text();
         news_creator[i] = $($(articles[i]).find('dc\\:creator')[0]).text();
         news_pubDate[i] = $($(articles[i]).find('pubDate')[0]).text();
     }
+    console.log(news_title);
+    console.log(news_description);
+    console.log(news_link);
+    console.log(news_creator);
+    console.log(news_pubDate);
 });
 
 //exports.news_maxLength = news_maxLength;
