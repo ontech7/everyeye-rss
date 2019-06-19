a338efa784e4ef606a273a5bfa6d5985f636e4f7
ontech7 <dontrok1@hotmail.it>
2019-06-20 00:13:41 +0200

    Added 'simulation' flag for tests - Fixed array names from singular to plural

--- index.js
+++ index.js
@@ -7,14 +7,17 @@ var patterns = [/(<!\[CDATA\[|\]\]>)/g,
     /(<!\[CDATA\[|\]\]>|<[^br].*?>|<br\/>|<br><br><a.*?>.*?<\/a>)/g,
     /<br><br>/g];
 
-var rss = async function(maxLength) {
-    var dom = await JSDOM.fromURL("https://www.everyeye.it/feed/feed_news_rss.asp", { referrer: "https://www.everyere.it/feed/feed_news_rss.asp" });
+var rss = async function(maxLength, simulation) {
+    if(simulation == null || !simulation)
+        var dom = await JSDOM.fromURL("https://www.everyeye.it/feed/feed_news_rss.asp", { referrer: "https://www.everyere.it/feed/feed_news_rss.asp" });
+    else
+        var dom = await JSDOM.fromFile("simulation.xml", {  });
 
-    var news = { title: [],
-                 description: [],
-                 link: [],
-                 creator: [],
-                 pubDate: [] };
+    var news = { titles: [],
+                 descriptions: [],
+                 links: [],
+                 creators: [],
+                 pubDates: [] };
 
     const $ = cheerio.load(dom.serialize(), { decodeEntities: false });
 
@@ -22,11 +25,11 @@ var rss = async function(maxLength) {
     var length = maxLength > articles.length ? articles.length : maxLength;
 
     for(var i = length - 1; i >= 0; i--) {
-        news.title[i] = ($($(articles[i]).find('title')[0]).html()).replace(patterns[0], "");
-        news.description[i] = ((($($(articles[i]).find('description')[0]).html()).replace(patterns[1], "")).replace(patterns[2], "\n")).replace(/<br>/g, "");
-        news.link[i] = $($(articles[i]).find('guid')[0]).text();
-        news.creator[i] = $($(articles[i]).find('dc\\:creator')[0]).text();
-        news.pubDate[i] = $($(articles[i]).find('pubDate')[0]).text();
+        news.titles[i] = ($($(articles[i]).find('title')[0]).html()).replace(patterns[0], "");
+        news.descriptions[i] = ((($($(articles[i]).find('description')[0]).html()).replace(patterns[1], "")).replace(patterns[2], "\n")).replace(/<br>/g, "");
+        news.links[i] = $($(articles[i]).find('guid')[0]).text();
+        news.creators[i] = $($(articles[i]).find('dc\\:creator')[0]).text();
+        news.pubDates[i] = $($(articles[i]).find('pubDate')[0]).text();
     }
 
     return new Promise(function(resolve, reject) { resolve(news) });
